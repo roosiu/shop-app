6d5929e1cd02eeb9aabbb396cbb6aa1e8ca9ad09
monika-sulecka <120500604+monika-sulecka@users.noreply.github.com>
2024-07-14 23:20:09 +0200

    Bug fix for displaying products after loading

--- src/components/ProductCatalog.jsx
+++ src/components/ProductCatalog.jsx
@@ -11,37 +11,34 @@ function ProductCatalog({ addToCart }) {
 			.catch(error => console.error('Error fetching products:', error))
 	}, [])
 
-	const addToCart = product => {
-		// Logika dodania produktu do koszyka
-		setMessage('Twój produkt jest w koszyku')
-		setTimeout(() => {
-			setMessage('')
-		}, 3000)
-	}
+	const handleAddToCart = (product) => {
+    addToCart(product);
+    setMessage('Twój produkt jest w koszyku');
+    setTimeout(() => {
+      setMessage('');
+    }, 3000);
+  };
 
 	return (
 		<div>
-			<h2 className='text-2xl font-bold mb-4'>Nasze bestsellery</h2>
-			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-				{products.map(product => (
-					<div key={product.id} className='border rounded-lg p-4 shadow-md'>
-						<img src={product.image} alt={product.name} className='w-full h-48 object-cover mb-4' />
-						<h3 className='font-bold text-lg'>{product.name}</h3>
-						<p className='text-gray-600'>{product.category}</p>
-						<p className='text-green-600 font-bold'>{product.price}</p>
-						<button
-							onClick={() => addToCart(product)}
-							className='mt-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700'>
-							Dodaj do koszyka
-						</button>
-						<div>
-							{message && <div className='message'>{message}</div>}
-							{/* Kod wyświetlający produkty */}
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
+      <h2 className='text-2xl font-bold mb-4'>Nasze bestsellery</h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        {products.map(product => (
+          <div key={product.id} className='border rounded-lg p-4 shadow-md'>
+            <img src={product.image} alt={product.name} className='w-full h-48 object-cover mb-4' />
+            <h3 className='font-bold text-lg'>{product.name}</h3>
+            <p className='text-gray-600'>{product.category}</p>
+            <p className='text-green-600 font-bold'>{product.price}</p>
+            <button
+              onClick={() => handleAddToCart(product)}
+              className='mt-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700'>
+              Dodaj do koszyka
+            </button>
+          </div>
+        ))}
+      </div>
+      {message && <div className='message'>{message}</div>}
+    </div>
 	)
 }
 
