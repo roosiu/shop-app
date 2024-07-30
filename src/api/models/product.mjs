0d3a6915263f2baac7c190dcc90c616355087c07
roosiu <roosiu@gmail.com>
2024-07-30 20:24:02 +0200

    refactor: Removed unused dependencies

--- src/api/models/product.mjs
+++
@@ -1,9 +0,0 @@
-// importuję mongoose
-const mongoose = require('mongoose')
-
-// tworzę schemat produktu - pola z których się składa produkt
-const productSchema = mongoose.Schema({
-	name: String,
-	price: Number,
-})
-module.exports = mongoose.model('Product', productSchema)
--- src/components/ProductCatalog.jsx
+++ src/components/ProductCatalog.jsx
@@ -1,45 +1,49 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
+function ProductCatalog({addToCart}) {
+    const [products, setProducts] = useState([])
+    const [message, setMessage] = useState('')
 
-function ProductCatalog({ addToCart }) {
-	const [products, setProducts] = useState([])
-	const [message, setMessage] = useState('')
+    useEffect(() => {
+        fetch('https://api.jsonbin.io/v3/b/6697d477e41b4d34e41334b5', {
+            headers: {
+                'x-access-key': import.meta.env.VITE_X_ACCESS_KEY,
+            }
+        })
+            .then(response => response.json())
+            .then(data => setProducts(data.record.products))
+            .catch(error => console.error('Error fetching products:', error));
+    }, [])
 
-	useEffect(() => {
-		fetch('http://localhost:3001/products')
-			.then(response => response.json())
-			.then(data => setProducts(data))
-			.catch(error => console.error('Error fetching products:', error))
-	}, [])
+    const handleAddToCart = (product) => {
+        addToCart(product);
+        setMessage('Twój produkt jest w koszyku');
+        setTimeout(() => {
+            setMessage('');
+        }, 3000);
+    };
 
-	const handleAddToCart = (product) => {
-    addToCart(product);
-    setMessage('Twój produkt jest w koszyku');
-    setTimeout(() => {
-      setMessage('');
-    }, 3000);
-  };
-
-	return (
-		<div>
-      <h2 className='text-2xl font-bold mb-4'>Nasze bestsellery</h2>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-        {products.map(product => (
-          <div key={product.id} className='border rounded-lg p-4 shadow-md'>
-            <img src={product.image} alt={product.name} className='w-full h-48 object-cover mb-4' />
-            <h3 className='font-bold text-lg'>{product.name}</h3>
-            <p className='text-gray-600'>{product.category}</p>
-            <p className='text-green-600 font-bold'>{product.price}</p>
-            <button
-              onClick={() => handleAddToCart(product)}
-              className='mt-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700'>
-              Dodaj do koszyka
-            </button>
-          </div>
-        ))}
-      </div>
-      {message && <div className='message'>{message}</div>}
-    </div>
-	)
+    return (
+        <div>
+            <h2 className='text-2xl font-bold mb-4'>Nasze bestsellery</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {
+                    products.map(product => (
+                        <div key={ product.id } className='border rounded-lg p-4 shadow-md'>
+                            <img src={ product.image } alt={ product.name } className='w-full h-48 object-cover mb-4'/>
+                            <h3 className='font-bold text-lg'>{ product.name }</h3>
+                            <p className='text-gray-600'>{ product.category }</p>
+                            <p className='text-green-600 font-bold'>{ product.price }</p>
+                            <button
+                                onClick={ () => handleAddToCart(product) }
+                                className='mt-4 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700'>
+                                Dodaj do koszyka
+                            </button>
+                        </div>
+                    )) }
+            </div>
+            { message && <div className='message'>{ message }</div> }
+        </div>
+    )
 }
 
 export default ProductCatalog
